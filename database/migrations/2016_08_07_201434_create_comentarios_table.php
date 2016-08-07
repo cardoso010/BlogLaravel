7a144f393a44e33b2bba97ee201ea8309c4ad238
Gabriel Cardoso <gabriel.cardoso010@gmail.com>
2016-08-07 17:24:23 -0300

    adicionando migration de comentarios

---
+++ database/migrations/2016_08_07_201434_create_comentarios_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateComentariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comentarios', function (Blueprint $table) {
+            $table->increments('id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('artigo_id')->references('id')->on('artigos');
+            $table->text('comentario');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('comentarios');
+    }
+}
