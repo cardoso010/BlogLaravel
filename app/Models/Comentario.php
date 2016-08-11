bb73879a4417c07722edfd8e23930909e94a65f9
Gabriel Cardoso <gabriel.cardoso010@gmail.com>
2016-08-11 12:59:42 -0300

    Model do comentario

--- app/Models/Comentario.php
+++ app/Models/Comentario.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comentario extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'artigo_id',
+        'comentario'
+    ];
 }
