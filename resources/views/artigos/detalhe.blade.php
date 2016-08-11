825984392e894e2bff8249a01f36b7cca79e5fe2
Gabriel Cardoso <gabriel.cardoso010@gmail.com>
2016-08-11 13:04:03 -0300

    formulario de comentario

--- resources/views/artigos/detalhe.blade.php
+++ resources/views/artigos/detalhe.blade.php
@@ -2,7 +2,37 @@
 
 @section('content')
 
-<h2>{{$artigo->nome}}</h2>
-<p>{{$artigo->descricao}}</p>
+<div class="row">
+    <div class="col-md-8 col-md-offset-2">
+        <div class="panel panel-default">
+            <div class="panel-heading"><h3>{{$artigo->nome}}</h3></div>
+            <div class="panel-body">
+                <p>{{$artigo->descricao}}</p>
+            </div>
+        </div>
+          <div class="bs-component">
+            <div class="modal">
+              <div class="modal-dialog">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h4 class="modal-title">Comente aqui!</h4>
+                  </div>
+                  <form method="post" action="{{ url('/comentarios/save') }}">
+                      {!! csrf_field() !!}
+                      <input type="hidden" name="artigo_id" value="{{ $artigo->id }}" />
+                      <div class="modal-body">
+                        <textarea class="form-control" rows="3" id="comentario" name="comentario"></textarea>
+                      </div>
+                      <div class="modal-footer">
+                        <button type="submit" class="btn btn-primary">Salvar</button>
+                      </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 
 @endsection
